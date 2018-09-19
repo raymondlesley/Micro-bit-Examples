bfda1055fd1babd4524c547caf14320ca4c4ccf4
Raymond Lesley <raymond.lesley@eon.com>
2018-09-19 13:26:24 +0100

    update (JS library change?)

---
+++ pong.js
@@ -0,0 +1,16 @@
+﻿// Pong - simple "tennis" game
+
+while (true) {
+    for (let i = 0; i <= 4; i = i + 1) {
+        basic.pause(100);
+        led.plot(i, 2);
+        basic.pause(100);
+        led.unplot(i - 1, 2);
+    }
+    for (let i = 4; i >= 0; i = i - 1) {
+        basic.pause(100);
+        led.plot(i, 2);
+        basic.pause(100);
+        led.unplot(i + 1, 2);
+    }
+}
