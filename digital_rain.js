5585bffb1777d13d671c00a0065f78f0291274bc
Raymond <raymond.lesley@eon.com>
2018-09-21 13:17:31 +0100

    update to be closer to C++ implementation

--- digital_rain.js
+++ digital_rain.js
@@ -1,7 +1,5 @@
 // DigitalRain - Matrix-style
 
-basic.clearScreen()
-
 function drawRaindrop(x: number, y: number, length: number) {
     // raindrop + tail
     for (let pos = 0; pos < length; pos++) {
@@ -20,11 +18,12 @@ function drawRaindrop(x: number, y: number, length: number) {
 function animateRaindrop(x: number, length: number, speed: number) {
     for (let head = 0; head < length + 5; head++) {
         drawRaindrop(x, head, length);
-        basic.pause(1000 / speed);
+        basic.pause(500 / speed);
     }
 }
 
 function animateRaindrops(x: number) {
+    basic.pause(Math.random(4) * 200 + 200);
     while (1) {
         let length = Math.random(6) + 2;
         let speed = Math.random(4) + 1;
@@ -33,19 +32,17 @@ function animateRaindrops(x: number) {
     }
 }
 
+function animateRaindrops_0() { animateRaindrops(0) }
+function animateRaindrops_1() { animateRaindrops(1) }
+function animateRaindrops_2() { animateRaindrops(2) }
+function animateRaindrops_3() { animateRaindrops(3) }
+function animateRaindrops_4() { animateRaindrops(4) }
+
+basic.clearScreen()
+
 // drop rain!
-control.inBackground(() => {
-    animateRaindrops(0);
-})
-control.inBackground(() => {
-    animateRaindrops(1);
-})
-control.inBackground(() => {
-    animateRaindrops(2);
-})
-control.inBackground(() => {
-    animateRaindrops(3);
-})
-control.inBackground(() => {
-    animateRaindrops(4);
-})
+control.inBackground(animateRaindrops_0);
+control.inBackground(animateRaindrops_1);
+control.inBackground(animateRaindrops_2);
+control.inBackground(animateRaindrops_3);
+control.inBackground(animateRaindrops_4);
